0df4a707d9fc91033d16684e96bf89864211706a
kenvarasolivo <kenvara.solivo@gmail.com>
2025-06-23 15:53:49 +0200

    Delete M6/praktikum_6/views directory

--- M6/praktikum_6/views/debug.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>This is a blade view showing phpinfo();</h1>
-<p><a href="/">go back</a>.</p>
-
-@php
-phpinfo();
-@endphp
--- M6/praktikum_6/views/examples/m4_7a_queryparameter.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.layout')
-
-@section('content')
-<p>Der Wert von name lautet: {{ $name }}</p>
-@endsection
